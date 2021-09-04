3452e237452490b3ecf2b198de3c3d318ee63592
Yan Qiao Yu <yqy1160058763@qq.com>
2021-09-04 10:14:01 +0800

    绘制登录背景样式
    绘制登录背景样式

--- ytoolsbox-vue/src/components/Login.vue
+++ ytoolsbox-vue/src/components/Login.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="login_container">
-      登录界面
+      <!-- 登录界面 -->
+
+      <!-- 这个是整个页面的容器 -->
+      <div class="login_box">
+        <!-- 这个是登录的白色的框的容器 -->
+        <div class="login_image">
+          <!-- 这个是登录的图片 -->
+          <img src="../assets/logo.png" alt="">
+        </div>
+      </div>
   </div>
 </template>
 
@@ -10,10 +19,55 @@ export default {
 }
 </script>
 
+<style lang="less" scoped>
 // lang=less，使得代码样式支持less语法
 // scope限定样式只在当前组件生效
-<style lang="less" scoped>
+
+// 登录页面的整个容器
 .login_container{
   background-color: #2b4b6b;
+  height: 100%;
+}
+
+// 中间那个白色的框
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  // 位置采用绝对位置
+  position: absolute;
+  // 举例顶部和左边都有50%的差距
+  left: 50%;
+  top: 50%;
+  // 然后再向左，向上位移50%
+  transform: translate(-50%,-50%);
+}
+
+.login_image{
+  width: 130px;
+  height: 130px;
+  // 给这个图片添加一个正方形的灰色边框
+  border: solid #eee;
+  // 把边框变成圆角
+  border-radius: 50%;
+  //图片和边框之间有间距，5px
+  padding: 5px;
+  // 添加阴影
+  box-shadow: 0 0 10px #ddd;
+
+  // 移动这个图片
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  top: -25%;
+  background-color: #fff;
+
+  img{
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
 }
 </style>
--- ytoolsbox-vue/src/main.js
+++ ytoolsbox-vue/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 

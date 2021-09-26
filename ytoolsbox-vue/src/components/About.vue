036031a473c8b55d04f4d0a4cb10870260d68258
Yan Qiao Yu <yqy1160058763@qq.com>
2021-09-26 20:44:05 +0800

    开始进行核心工具盒模块的编写
    开始进行核心工具盒模块的编写

--- ytoolsbox-vue/src/components/About.vue
+++ ytoolsbox-vue/src/components/About.vue
@@ -3,7 +3,7 @@
     <!-- 面包屑路径 -->
     <el-breadcrumb>
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>工具盒</el-breadcrumb-item>
+      <el-breadcrumb-item>关于</el-breadcrumb-item>
     </el-breadcrumb>
   </div>
 </template>
--- ytoolsbox-vue/src/components/ToolBox.vue
+++ ytoolsbox-vue/src/components/ToolBox.vue
@@ -5,11 +5,75 @@
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>工具盒</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 卡片信息 -->
+    <el-card class="box-card">
+      <el-card
+        v-for="o in 10"
+        :key="o"
+        :body-style="{ padding: '0px' }"
+        class="toolbox"
+      >
+        <!-- <img
+          src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+          class="image"
+        /> -->
+        <div style="padding: 14px;">
+          <span>好吃的汉堡</span>
+          <div class="bottom clearfix">
+            <time class="time">{{ currentDate }}</time>
+            <el-button type="text" class="button">操作按钮</el-button>
+          </div>
+        </div>
+      </el-card>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+.toolbox {
+  width: 250px;
+  display: inline-block;
+  margin: 10px 10px;
+}
+
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: '';
+}
+
+.clearfix:after {
+  clear: both;
+}
+</style>

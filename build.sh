aa1db4338a424b63b88186cd06288905e973e03d
Yan Qiao Yu <yqy1160058763@qq.com>
2021-10-21 17:43:45 +0800

    uodate build

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ docker rm yToolsBox-api yToolsBox-dashboard yToolsBox-db
 docker images | grep none | awk '{print $3}' | xargs docker rmi
 cd ./ytoolsbox-vue
 npm run build
-docker rmi yanqiaoyu/ytoolsbox-dashboard:v0.1.1 yanqiaoyu/ytoolsbox-api:v0.1.1 -f
+docker rmi yanqiaoyu/ytoolsbox-dashboard:v0.2 yanqiaoyu/ytoolsbox-api:v0.2 -f
 cd ../
 docker-compose up -d
 
-docker push yanqiaoyu/ytoolsbox-dashboard:v0.1.1
-docker push yanqiaoyu/ytoolsbox-api:v0.1.1
+docker push yanqiaoyu/ytoolsbox-dashboard:v0.2
+docker push yanqiaoyu/ytoolsbox-api:v0.2

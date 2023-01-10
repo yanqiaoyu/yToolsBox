8eed50e7bcc8ace193c8eb31c06f609adc069f13
Yan Qiao Yu <yqy1160058763@qq.com>
2023-01-10 11:15:43 +0800

    删除主线代码

--- ytoolsbox-gin/dto/LoginDTO.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-// 登录的dto
-type LoginDTO struct {
-	UserName string `form:"name" json:"name"`
-	Password string `form:"password" json:"password"`
-}
--- ytoolsbox-gin/model/CommonModel.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Meta struct {
-	Msg         string `json:"msg"`
-	Status_code int    `json:"status_code"`
-}

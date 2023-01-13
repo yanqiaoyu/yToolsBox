29b82aa1fe8a5db303ef311431ca966f9bc7af35
Yan Qiao Yu <yqy1160058763@qq.com>
2023-01-13 17:57:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # 数据库容器的版本
 export DB_TAG=v0.0.1
 # API容器的版本
-export API_TAG=v0.1.17
+export API_TAG=v0.2.12
 # 前端容器的版本
-export DASHBOARD_TAG=v0.1.16
+export DASHBOARD_TAG=v0.2.12
 # 对接大脑的容器的版本
-export REQ_TAG=v0.0.5
+export REQ_TAG=v0.2.11
 # 图像识别服务的版本
 export RECOGNIZE_TAG=v0.1
 # 回放服务的版本
@@ -17,3 +17,4 @@ cd ../
 docker-compose up -d
 docker push yanqiaoyu/ytoolsbox-api-custom:${API_TAG}
 docker push yanqiaoyu/ytoolsbox-dashboard-custom:${DASHBOARD_TAG}
+docker push yanqiaoyu/ytoolsbox-req-custom:${REQ_TAG}

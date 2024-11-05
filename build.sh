20b342d921ce1f9ab3c0a0924f340e25d619dad7
Yan Qiao Yu <yqy1160058763@qq.com>
2024-11-05 15:41:34 +0800

    更新build文件

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
+# 获取编译时间（格式：YYYYMMDDHHMMSS）
+BUILD_TIME=$(date +"%Y%m%d%H%M%S")
+
 # 数据库容器的版本
 export DB_TAG=v0.0.1
 # API容器的版本
-export API_TAG=v3.0.18
+export API_TAG="v3.0.21-${BUILD_TIME}"
 # 前端容器的版本
-export DASHBOARD_TAG=v3.0.18
+export DASHBOARD_TAG="v3.0.21-${BUILD_TIME}"
 # 对接大脑的容器的版本
 export REQ_TAG=v3.0.18
 # 图像识别服务的版本
